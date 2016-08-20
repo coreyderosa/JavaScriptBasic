5e79df0cd4f0beae597ffd8aecbbce940660bcc0
coreyderosa <coreyderosa@gmail.com>
2016-08-20 15:46:13 -0600

    Exercise 3 compete
    3. Write a JavaScript program to get the current date.
    Expected Output :
    mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy

---
+++ Exercise3.js
@@ -0,0 +1,10 @@
+
+function currentDate() {
+	
+	var date = new Date();
+	var MM = date.getMonth() + 1;
+	var DD = date.getDate();
+	var YYYY = date.getFullYear();
+		document.write(MM + "/" + DD + "/" + YYYY);
+}
+
